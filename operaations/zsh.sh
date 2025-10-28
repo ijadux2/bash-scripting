e2673c81a78b365fa21946d535f99d3ffb692aeb
kumar vaibhav <kv26102009@gmail.com>
2025-10-28 19:59:32 +0530

    extraz

---
+++ operaations/zsh.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/bash
+
+# edit this lines for installations of shell:zsh
+#sudo dnf install zsh
+
+# this is for installations of zsh in fedora !!
+#
+# zsh config in bash
+#
+#
+# zsh in bash-scrpiting
+
+read -p "shell-zsh (y,n,) >> " source
+
+if [[ $source == y ]]; then
+  #source ~/.zshrc
+  zsh
+else
+  command chsh -s $(which zsh)
+  zsh
+fi
+
+# zsh config for shell
+source ~/.zshrc
+#export ZSH="$HOME/. oh-my-zsh"
+#ZSH_THEME="robbyrussel"
+#plugins=(
+#  zsh-autosuggestions
+#  zsh-syntax-highlighting
+#  zoxide
+#  starship
+#)
+#
+#aliases=(
+#  cd="z"
+#  v="nvim"
+#  ls="lsd"
+#  rm="rm -rf"
+#  e="exit"
+#)
